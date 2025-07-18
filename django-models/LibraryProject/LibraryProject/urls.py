1477deaee83328c8c4e54bb9dbfa816b7e0dc269
Bongi90 <avalashiya90@gmail.com>
2025-07-18 15:49:59 +0200

    Ensure Library model is imported in views.py for checks

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('relationship/', include('relationship_app.urls')),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,31 @@
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Library
+from .models import Book, Library, Author 
+
+def list_books(request):
+    """
+    Retrieves all books from the database and renders them in a list_books.html template.
+    """
+    books = Book.objects.all().order_by('title') 
+    context = {
+        'books': books
+    }
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
+    """
+    Displays details for a specific Library instance, including all books associated with it.
+    Uses Django's DetailView generic class-based view.
+    """
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library' 
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds extra context to the template, if needed.
+        For DetailView, the object itself (library) is automatically passed.
+        """
+        context = super().get_context_data(**kwargs)
+        return context
 
