0f10516e8ede7f738208f6276d8cf76eff499bcb
Bongi90 <avalashiya90@gmail.com>
2025-07-18 16:35:12 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin_view/', views.admin_view, name='admin_view'),
-    path('librarian_view/', views.librarian_view, name='librarian_view'),
-    path('member_view/', views.member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
 ]
+
 
