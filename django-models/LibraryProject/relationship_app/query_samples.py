f21f2e4b33c0cbf9cb2fd665854cb2d390f07ba1
Bongi90 <avalashiya90@gmail.com>
2025-07-18 15:06:41 +0200

    Fix: use library_name variable as required by checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,21 @@
 import os
 import django
 
-# Setup Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-models.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-author = Author.objects.get(name="John Doe")
+author_name = "John Doe"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print("Books by John Doe:", [book.title for book in books_by_author])
+print("Books by", author_name, ":", [book.title for book in books_by_author])
 
-# List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", [book.title for book in books_in_library])
+print("Books in", library_name, ":", [book.title for book in books_in_library])
 
-# Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
 print(f"Librarian for {library.name}: {librarian.name}")
+
