60617fc24e080520de672cac12fc0f61e4c4ede1
Bongi90 <avalashiya90@gmail.com>
2025-07-27 08:24:31 +0200

    Implement custom user model with date_of_birth and profile_photo fields

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,34 +1,26 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.conf import settings 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def __str__(self):
-        return self.name
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    biography = models.TextField(blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
-    publication_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
-
-    borrowed_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-    return_date = models.DateField(null=True, blank=True)
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.title
+        return self.username
