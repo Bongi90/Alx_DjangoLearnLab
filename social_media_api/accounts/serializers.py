47b8893f4f9a6740c66acc142803b299bb6e9654
Bongi90 <avalashiya90@gmail.com>
2025-08-24 02:02:31 +0200

    Completed user authentication task, fixed serializer code

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,32 +1,34 @@
-# social_media_api/accounts/serializers.py
-
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from .models import CustomUser
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    
+
     class Meta:
         model = CustomUser
         fields = ['username', 'password', 'email', 'bio']
-        
+
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
         user.bio = validated_data.get('bio', '')
         user.save()
-        return user
         
+        # Create a token for the new user
+        Token.objects.create(user=user)
+        
+        return user
+
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
     token = serializers.CharField(read_only=True)
-    
+
     def validate(self, data):
         user = authenticate(username=data['username'], password=data['password'])
         if user and user.is_active:
